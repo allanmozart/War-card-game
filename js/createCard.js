3e7de10cd88afd6eb24d19ffc7cc32c736d0c95e
AndrejBerezni <74119993+AndrejBerezni@users.noreply.github.com>
2023-09-08 15:37:56 +0100

    fix card bottom value in css

--- js/createCard.js
+++ js/createCard.js
@@ -1,23 +1,3 @@
-// const createCard1 = (name, color, symbol, id) => {
-//   const card = document.createElement("div");
-
-//   card.id = id;
-//   card.classList.add("card");
-
-//   const cardValue = document.createElement("h1");
-//   const cardSymbol = document.createElement("h1");
-
-//   cardValue.innerText = name;
-//   cardSymbol.innerText = symbol;
-//   cardValue.style.color = color;
-//   cardSymbol.style.color = color;
-
-//   card.appendChild(cardValue);
-//   card.appendChild(cardSymbol);
-
-//   return card;
-// };
-
 const createCard = (cardObject, id) => {
   const card = document.createElement("div");
   card.id = id;
