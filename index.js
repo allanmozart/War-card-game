a05696b14eb5cd211501c5ebadb2538333419f69
AndrejBerezni <74119993+AndrejBerezni@users.noreply.github.com>
2023-09-08 09:36:18 +0100

    fit navbar js logic into main js code

--- index.js
+++ index.js
@@ -1,51 +1,51 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+// const hamburger = document.querySelector(".hamburger");
+// const navMenu = document.querySelector(".nav-menu");
 
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle('active');
-    navMenu.classList.toggle('active');
-})
+// hamburger.addEventListener("click", () => {
+//   hamburger.classList.toggle("active");
+//   navMenu.classList.toggle("active");
+// });
 
-document.addEventListener("DOMContentLoaded", function() {
-    const startNewGameLink = document.getElementById("startNewGame");
-    const modal = document.getElementById("myModal");
-    const closeModal = document.getElementById("closeModal");
-    const gameNameInput = document.getElementById("gameNameInput");
-    //const gameBoard = document.querySelector(".game-board");
-    const gameBoard = document.getElementById("player-name");
-    const confirmButton = document.getElementById("confirmGameName");
-  
-    startNewGameLink.addEventListener("click", function(event) {
-      event.preventDefault();
-      modal.style.display = "block";
-    });
-  
-    closeModal.addEventListener("click", function() {
-      modal.style.display = "none";
-    });
-  
-    // Fechar o modal se o usuário clicar fora dele
-    window.addEventListener("click", function(event) {
-      if (event.target === modal) {
-        modal.style.display = "none";
-      }
-    });
-  
-    // Adicionar valor ao último <li> quando o usuário confirmar
-    confirmButton.addEventListener("click", function() {
-      const gameName = gameNameInput.value.trim(); // Remova espaços em branco
-      if (gameName) {
-        const lastListItem = gameBoard;
-            lastListItem.textContent= gameName     
-        modal.style.display = "none";
-        gameNameInput.value = ""; // Limpa o campo de entrada
-      }
-    });
-  
-    // Adicionar evento de tecla para capturar Enter
-    gameNameInput.addEventListener("keyup", function(event) {
-      if (event.key === "Enter") {
-        confirmButton.click(); // Clique no botão Confirm quando Enter for pressionado
-      }
-    });
-  });
+// document.addEventListener("DOMContentLoaded", function () {
+//   const startNewGameLink = document.getElementById("startNewGame");
+//   const modal = document.getElementById("myModal");
+//   const closeModal = document.getElementById("closeModal");
+//   const gameNameInput = document.getElementById("gameNameInput");
+//   //const gameBoard = document.querySelector(".game-board");
+//   const gameBoard = document.getElementById("player-name");
+//   const confirmButton = document.getElementById("confirmGameName");
+
+//   startNewGameLink.addEventListener("click", function (event) {
+//     event.preventDefault();
+//     modal.style.display = "block";
+//   });
+
+//   closeModal.addEventListener("click", function () {
+//     modal.style.display = "none";
+//   });
+
+//   // Fechar o modal se o usuário clicar fora dele
+//   window.addEventListener("click", function (event) {
+//     if (event.target === modal) {
+//       modal.style.display = "none";
+//     }
+//   });
+
+//   // Adicionar valor ao último <li> quando o usuário confirmar
+//   confirmButton.addEventListener("click", function () {
+//     const gameName = gameNameInput.value.trim(); // Remova espaços em branco
+//     if (gameName) {
+//       const lastListItem = gameBoard;
+//       lastListItem.textContent = gameName;
+//       modal.style.display = "none";
+//       gameNameInput.value = ""; // Limpa o campo de entrada
+//     }
+//   });
+
+//   // Adicionar evento de tecla para capturar Enter
+//   gameNameInput.addEventListener("keyup", function (event) {
+//     if (event.key === "Enter") {
+//       confirmButton.click(); // Clique no botão Confirm quando Enter for pressionado
+//     }
+//   });
+// });
--- js/main.js
+++ js/main.js
@@ -4,23 +4,68 @@ import performAnimation from "./cardAnimations.js";
 import createCard from "./createCard.js";
 import { winGame, removeWinGame } from "./winGame.js";
 
-const newGame = document.getElementById("new");
+// Navbar:
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+const modal = document.getElementById("myModal");
+const closeModal = document.getElementById("closeModal");
+const gameNameInput = document.getElementById("gameNameInput");
+const gameBoard = document.getElementById("player-name");
+const confirmButton = document.getElementById("confirmGameName");
+
+// startNewGameLink.addEventListener("click", function (event) {
+//   event.preventDefault();
+//   modal.style.display = "block";
+// });
+
+closeModal.addEventListener("click", function () {
+  modal.style.display = "none";
+});
+
+// Fechar o modal se o usuário clicar fora dele
+window.addEventListener("click", function (event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+});
+
+// Adicionar valor ao último <li> quando o usuário confirmar
+confirmButton.addEventListener("click", function () {
+  const gameName = gameNameInput.value.trim(); // Remova espaços em branco
+  if (gameName) {
+    const lastListItem = gameBoard;
+    lastListItem.textContent = gameName;
+    modal.style.display = "none";
+    gameNameInput.value = ""; // Limpa o campo de entrada
+  }
+});
+
+// Adicionar evento de tecla para capturar Enter
+gameNameInput.addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
+    confirmButton.click(); // Clique no botão Confirm quando Enter for pressionado
+  }
+});
+const newGame = document.getElementById("startNewGame");
 const gameDiv = document.querySelector("main");
 const pDeck = document.getElementById("player-deck");
 const playerScore = document.getElementById("player-score");
 const computerScore = document.getElementById("computer-score");
 
-const symbols = {
-  spades: "♠",
-  hearts: "♥",
-  diamonds: "♦",
-  clubs: "♣",
-};
-
 let game;
 
 newGame.addEventListener("click", () => {
   removeWinGame(); // If there a window stating the winner, remove it
+  modal.style.display = "block";
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
   game = new Game(deck);
   playerScore.innerText = `Player: 26`;
   computerScore.innerText = `Computer: 26`;
